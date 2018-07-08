857a01d3dd71da03da812b44dd72a87b40c623df
Michael Henretty <michael.henretty@gmail.com>
2018-07-08 05:49:25 +0200

    add kinto framework

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-document.body.textContent = 'Hello world!';
+import Kinto from './vendor/kinto.min.js';
+const username = 'admin';
+const password = 'password';
+const kinto = new Kinto({
+  remote: 'http://kinto.mozvoice.org/v1'
+  headers: {
+    Authorization: "Basic " + btoa(`${username}:${password}`),
+  },
+  bucket: 'new',
+});
+
+const articles = kinto.collection("articles");
+
+articles.create({title: "foo"})
+  .then(console.log.bind(console))
+  .catch(console.error.bind(console));
