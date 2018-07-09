09b2fd5150082c642725deec8ad858a512958f4e
Michael Henretty <michael.henretty@gmail.com>
2018-07-10 01:42:10 +0200

    always use index as the starting point

--- web/index.jsx
+++ web/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM  from "react-dom";
 
 import App from './components/app';
+import './index.css';
 
 const USERNAME = 'admin';
 const PASSWORD = 'password';
