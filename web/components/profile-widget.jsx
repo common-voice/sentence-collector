1173d27a24982e632650c20f8128af2c6bed054c
Michael Henretty <michael.henretty@gmail.com>
2018-08-05 04:30:22 +0200

    pretty-up the profile widget

--- web/components/profile-widget.jsx
+++ web/components/profile-widget.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { NavLink, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import { logout, isLoggedIn } from '../store/actions';
 
@@ -20,7 +20,7 @@ class ProfileWidget extends React.Component {
     }
 
     return <div className="profile-widget">
-      Welcome {this.props.username}!
+      <h2><Link to="/profile">{this.props.username}</Link></h2>
       <button className="inverse" onClick={this.onLogout}>Logout</button>
     </div>;
   }
