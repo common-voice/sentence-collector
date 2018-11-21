ba6ac9f12c97466140cbdee8387941c8d02de398
Michael Kohler <me@michaelkohler.info>
2018-11-21 19:56:05 +0100

    Add link to Public Domain

--- web/src/components/pages/add.jsx
+++ web/src/components/pages/add.jsx
@@ -296,7 +296,8 @@ const ConfirmForm = (props) => (
         sentences are already reviewed. Great job!
       </p>
     )}
-    <p><b>{`${props.ready.length} sentences ready for submission! By submitting these sentences you grant a public domain license for self-written sentences, or declare that sentences from a third-party are under Public Domain License and can be used.`}</b></p>
+    <p><b>{`${props.ready.length} sentences ready for submission!`}</b></p>
+    <p>By submitting these sentences you grant a <a href="https://en.wikipedia.org/wiki/Public_domain" target="_blank">Public Domain License</a> for self-written sentences, or declare that sentences from a third-party are under Public Domain License and can be used.</p>
     <section id="confirm-buttons">
       <button type="submit">Confirm</button>
       <button onClick={props.onCancel}>Cancel</button>
