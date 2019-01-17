adc0ea60f4400417da62350f9d7bd2a5e96b86a4
Michael Kohler <me@michaelkohler.info>
2019-01-17 21:48:22 +0100

    feat: add zh-TW, zh-CN, zh-HK (issue #76)

--- shared/languages.js
+++ shared/languages.js
@@ -2,9 +2,54 @@ import ISO6391 from 'iso-639-1';
 
 export const ENGLISH_CODE = 'en';
 
-export const getAllLanguages =
-  () => ISO6391.getLanguages(ISO6391.getAllCodes());
+const ADDITIONAL_LANGUAGES = [
+  {
+    code: 'zh-CN',
+    name: 'Chinese - China',
+    nativeName: '中文 (中国)',
+  },
+  {
+    code: 'zh-TW',
+    name: 'Chinese - Taiwan',
+    nativeName: '中文 (台灣)',
+  },
+  {
+    code: 'zh-HK',
+    name: 'Chinese - Hong Kong',
+    nativeName: '中文 (香港)',
+  },
+];
 
-export const getLanguages = ISO6391.getLanguages.bind(ISO6391);
+const LANGUAGES_TO_REMOVE = [
+  'zh',
+];
 
-export const getLanguageName = ISO6391.getNativeName.bind(ISO6391);
+export const getAllLanguages = () => {
+  const isoLanguages = ISO6391.getLanguages(ISO6391.getAllCodes());
+  const allLanguages = changeLanguages(isoLanguages);
+  return allLanguages;
+};
+
+export const getLanguages = (codes) => {
+  const languages = ISO6391.getLanguages(codes);
+  return languages;
+};
+
+export const getLanguageName = (code) => {
+  const language = getAllLanguages().find((language) => language.code === code);
+
+  if (!language) {
+    return 'ERROR - UNKNOWN - ' + code;
+  }
+
+  return language.nativeName;
+};
+
+function changeLanguages(isoLanguages) {
+  const languagesWithoutRemoved = isoLanguages.filter((language) => {
+    return !LANGUAGES_TO_REMOVE.includes(language.code);
+  });
+
+  const allLanguages = languagesWithoutRemoved.concat(ADDITIONAL_LANGUAGES);
+  return allLanguages;
+}
