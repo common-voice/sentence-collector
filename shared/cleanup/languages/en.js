40d5be18927a302d85855888cfa1f4e73b88ebd9
Michael Kohler <me@michaelkohler.info>
2019-02-13 19:27:17 +0100

    fix: clean up spaces (fixes #153)

--- shared/cleanup/languages/en.js
+++ shared/cleanup/languages/en.js
@@ -1,3 +1,14 @@
 export function sortSentences(sentences) {
   return sentences.sort();
 }
+
+export function cleanSpaces(sentences) {
+  return sentences.map((sentence) => {
+    return sentence
+      .replace('  ', ' ')
+      .replace(' :', ':')
+      .replace(' ,', ',')
+      .replace(' .', '.')
+    ;
+  });
+}
