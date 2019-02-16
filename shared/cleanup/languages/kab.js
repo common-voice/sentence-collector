6f53fe0c5cee042a7327fd5e10030b52146f6f62
MozillaKab <7033740+mozillakab@users.noreply.github.com>
2019-02-16 19:36:35 +0100

    Create kab.js
    correct punctuation style

---
+++ shared/cleanup/languages/kab.js
@@ -0,0 +1,16 @@
+export function sortSentences(sentences) {
+  return sentences.sort();
+}
+
+export function cleanSpaces(sentences) {
+  return sentences.map((sentence) => {
+    return sentence
+      .replace('  ', ' ')
+      .replace(' :', ':')
+      .replace(' ,', ',')
+      .replace(' .', '.')
+      .replace(' ?', '?')
+      .replace(' !', '!')
+    ;
+  });
+}
