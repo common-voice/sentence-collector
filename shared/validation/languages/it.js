6b918bcf71fea0c36b507f85f8dcc530da0a64c0
jotaro-sama <36264038+jotaro-sama@users.noreply.github.com>
2019-03-19 00:00:15 +0100

    chore: fix it.js

--- shared/validation/languages/it.js
+++ shared/validation/languages/it.js
@@ -7,13 +7,13 @@ const MAX_WORDS = 17;
 
 // Numbers that are not allowed in a sentence depending on the language. For
 // Italian: tutti i numeri sono consentiti ma se e quando avremo la possibilità di fare regole personalizzate metteremo una condizione ulteriore sulla lunghezza se vi sono dei numeri nella frase.
-//const NUMBERS_REGEX = /[0-9]+/;
+const NUMBERS_REGEX = /[0-9]+/;
 
 /* eslint-disable-next-line no-useless-escape */
 // Italian: Simboli non permessi, aggiungere anche qui sotto oltre che nella regex:
 // “ ” ‘ ’ ( ) É 
 //doppio " " e più di un "." nella stessa frase.
-const SYMBOL_REGEX = /[“”‘’\(\)É]| {2,}|\..*\./       //  original:   /[<>\+\*\\#@\^\[\]\(\)\/]/;
+const SYMBOL_REGEX = /[“”‘’()É]| {2,}|\..*\./;       //  original:   /[<>\+\*\\#@\^\[\]\(\)\/]/;
 // Any words consisting of uppercase letters or uppercase letters with a period
 // inbetween are considered abbreviations or acronyms.
 // This currently also matches fooBAR but we most probably don't want that either
@@ -32,14 +32,14 @@ export function getMinLength() {
 
 export function filterNumbers(sentence) {
   //Italian: all numbers are allowed
-  //return !sentence.match(NUMBERS_REGEX);
-  return true;
+  var filter = !sentence.match(NUMBERS_REGEX);
+  return true || filter;
 }
 
 export function filterAbbreviations(sentence) {
   //Italian: non abbiamo regole particolari sulle abbreviazioni
-  //return !sentence.match(ABBREVIATION_REGEX);
-  return true;
+  var filter = !sentence.match(ABBREVIATION_REGEX);
+  return true || filter;
 }
 
 export function filterSymbols(sentence) {
