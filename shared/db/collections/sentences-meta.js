a30b035ffe8d16e1563b9b5af2f6014b3e24e619
Michael Kohler <me@michaelkohler.info>
2019-05-25 19:31:34 +0200

    chore: remove unused variable

--- shared/db/collections/sentences-meta.js
+++ shared/db/collections/sentences-meta.js
@@ -166,8 +166,6 @@ export default class SentencesMeta {
   }
 
   getAllRejectedByUsername(languages, username) {
-    const rejectedSentences = {};
-
     const promises = languages.map((language) => this.getAllUnapprovedByUsername(language, username));
 
     return Promise.all(promises)
