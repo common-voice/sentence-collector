f15899a31cc3dccc9856accce18ba4614c39116d
Alejandro Close <alejandroclose@Alejandros-MacBook-Pro.local>
2019-06-01 02:46:16 +0200

    test: sidebar mobile ui

---
+++ web/src/components/header-btn.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeaderBtn = (props) => {
+  return(
+    <button id="header-btn" onClick={props.handleClick.bind(this)}>
+      <div className={props.arrow}></div>
+    </button>
+  );
+}
+
+export default HeaderBtn;
--- web/src/components/page.jsx
+++ web/src/components/page.jsx
@@ -1,20 +1,46 @@
 import React from 'react';
 
 import Header from '../containers/header';
+import HeaderBtn from './header-btn';
+
+let DEFAULT_STATE = {
+  headerIsOpen: true,
+}
 
 export default class Page extends React.Component {
   constructor(props) {
     super(props);
+    this.state = DEFAULT_STATE;
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick () {
+    this.setState((prevState) => {
+      return {headerIsOpen : !prevState.headerIsOpen}
+    });
   }
 
   render() {
-    return [
-      <Header key="header" />,
-      <div id="page" key="page">
-        <main key="main">
-          {this.props.children}
-        </main>
-      </div>
-    ];
+    if (this.state.headerIsOpen) {
+      return [
+        <Header key="header" />,
+        <div id="page" key="page">
+          <HeaderBtn arrow="header-btn-arrow-left" handleClick={this.handleClick}/>
+          <main key="main">
+            {this.props.children}
+          </main>
+        </div>
+      ];
+
+    } else {
+      return [
+        <div id="page" key="page">
+          <HeaderBtn arrow="header-btn-arrow-right" handleClick={this.handleClick}/>
+          <main key="main">
+            {this.props.children}
+          </main>
+        </div>
+      ];
+    }
   }
 }
