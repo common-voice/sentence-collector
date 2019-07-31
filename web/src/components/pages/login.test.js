210e4816342f76dbd50a3b4707e57d0d4b5bb616
Jessie Anderson <housewifehacker@gmail.com>
2019-07-31 15:32:42 -0400

    build: add js tests with jest and enzyme (#244)

---
+++ web/src/components/pages/login.test.js
@@ -0,0 +1,23 @@
+import { mount } from 'enzyme';
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import ConnectedLogin from '../../containers/login';
+import { getStore } from '../../tests/testUtils';
+
+test('Login button is disabled without input values', () => {
+  const initialState = {
+    app: {
+      authed: false,
+      username: null,
+      errormessage: null,
+      loginDisabled: true,
+    },
+  };
+  let store = getStore(initialState);
+  let component = mount(<Provider store={store}><ConnectedLogin /></Provider>);
+
+  expect(component).toMatchSnapshot();
+  expect(component.find('form#login button').is('[disabled]')).toBeTruthy();
+});
+
