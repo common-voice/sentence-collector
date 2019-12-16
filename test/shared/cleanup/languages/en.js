b291ca88120ac23796c786441cabf39b7bef3548
Michael Kohler <me@michaelkohler.info>
2019-12-16 21:01:34 +0100

    chore: use macro for tests

--- test/shared/cleanup/languages/en.js
+++ test/shared/cleanup/languages/en.js
@@ -1,167 +1,35 @@
 import test from 'ava';
 import * as en from '../../../../shared/cleanup/languages/en';
 
-test('[EN] removes multiple spaces', t => {
-  const input = ['this is  wrong'];
-  const expected = ['this is wrong'];
+function clean(t, input, expected) {
+  const cleaned = en.clean(input);
+	t.deepEqual(cleaned, expected);
+}
 
-  const computed = en.clean(input);
-
-  t.deepEqual(computed, expected);
-});
-
-test('[EN] removes space before colon', t => {
-  const input = ['this is : wrong'];
-  const expected = ['this is: wrong'];
-
-  const computed = en.clean(input);
-
-  t.deepEqual(computed, expected);
-});
-
-test('[EN] removes space before comma', t => {
-  const input = ['this is , wrong'];
-  const expected = ['this is, wrong'];
-
-  const computed = en.clean(input);
-
-  t.deepEqual(computed, expected);
-});
-
-test('[EN] removes space before period', t => {
-  const input = ['this is wrong .'];
-  const expected = ['this is wrong.'];
-
-  const computed = en.clean(input);
-
-  t.deepEqual(computed, expected);
-});
-
-test('[EN] removes space before question mark', t => {
-  const input = ['this is wrong ?'];
-  const expected = ['this is wrong?'];
-
-  const computed = en.clean(input);
-
-  t.deepEqual(computed, expected);
-});
-
-test('[EN] removes space before exclamation mark', t => {
-  const input = ['this is wrong !'];
-  const expected = ['this is wrong!'];
-
-  const computed = en.clean(input);
-
-  t.deepEqual(computed, expected);
-});
-
-test('[EN] removes comma at beginning of sentence', t => {
-  const input = [',this is wrong'];
-  const expected = ['this is wrong'];
-
-  const computed = en.clean(input);
-
-  t.deepEqual(computed, expected);
-});
-
-test('[EN] removes comma at beginning of sentence with space', t => {
-  const input = [', this is wrong'];
-  const expected = ['this is wrong'];
-
-  const computed = en.clean(input);
-
-  t.deepEqual(computed, expected);
-});
-
-test('[EN] removes comma at end of sentence', t => {
-  const input = ['this is wrong,'];
-  const expected = ['this is wrong'];
-
-  const computed = en.clean(input);
-
-  t.deepEqual(computed, expected);
-});
-
-test('[EN] removes several multiple spaces', t => {
-  const input = ['this is  wrong  on two levels', 'this is  wrong       on two levels'];
-  const expected = ['this is wrong on two levels', 'this is wrong on two levels'];
-
-  const computed = en.clean(input);
-
-  t.deepEqual(computed, expected);
-});
-
-test('[EN] removes several multiple space before colon', t => {
-  const input = ['this is : wrong'];
-  const expected = ['this is: wrong'];
-
-  const computed = en.clean(input);
-
-  t.deepEqual(computed, expected);
-});
-
-test('[EN] removes several spaces before comma', t => {
-  const input = ['this is , wrong , too'];
-  const expected = ['this is, wrong, too'];
-
-  const computed = en.clean(input);
-
-  t.deepEqual(computed, expected);
-});
-
+test('[EN] removes multiple spaces', clean, ['this is  wrong'], ['this is wrong']);
+test('[EN] removes space before colon', clean, ['this is : wrong'], ['this is: wrong']);
+test('[EN] removes space before comma', clean, ['this is , wrong'], ['this is, wrong']);
+test('[EN] removes space before period', clean, ['this is wrong .'], ['this is wrong.']);
+test('[EN] removes space before question mark', clean, ['this is wrong ?'], ['this is wrong?']);
+test('[EN] removes space before exclamation mark', clean, ['this is wrong !'], ['this is wrong!']);
+test('[EN] removes comma at beginning of sentence', clean, [',this is wrong'], ['this is wrong']);
+test('[EN] removes comma at beginning of sentence with space', clean, [', this is wrong'], ['this is wrong']);
+test('[EN] removes comma at end of sentence', clean, ['this is wrong,'], ['this is wrong']);
+test('[EN] removes several multiple spaces', clean,
+  ['this is  wrong  on two levels', 'this is  wrong       on two levels'],
+  ['this is wrong on two levels', 'this is wrong on two levels'],
+);
+test('[EN] removes several multiple space before colon', clean, ['this is : wrong'], ['this is: wrong']);
+test('[EN] removes several spaces before comma', clean, ['this is , wrong , too'], ['this is, wrong, too']);
 // In theory it's invalid anyway but we never know if it eventually exists in SC.
-test('[EN] removes several spaces before period', t => {
-  const input = ['this is wrong . And wrong too .'];
-  const expected = ['this is wrong. And wrong too.'];
-
-  const computed = en.clean(input);
-
-  t.deepEqual(computed, expected);
-});
-
+test('[EN] removes several spaces before period', clean,
+  ['this is wrong . And wrong too .'], ['this is wrong. And wrong too.']);
 // In theory it's invalid anyway but we never know if it eventually exists in SC.
-test('[EN] removes several space before question mark', t => {
-  const input = ['this is wrong ? And wrong too ?'];
-  const expected = ['this is wrong? And wrong too?'];
-
-  const computed = en.clean(input);
-
-  t.deepEqual(computed, expected);
-});
-
+test('[EN] removes several space before question mark', clean,
+  ['this is wrong ? And wrong too ?'], ['this is wrong? And wrong too?']);
 // In theory it's invalid anyway but we never know if it eventually exists in SC.
-test('[EN] removes several spaces before exclamation mark', t => {
-  const input = ['this is wrong ! And wrong too !'];
-  const expected = ['this is wrong! And wrong too!'];
-
-  const computed = en.clean(input);
-
-  t.deepEqual(computed, expected);
-});
-
-test('[EN] removes multiple commas at beginning of sentence', t => {
-  const input = [',,,,this is wrong'];
-  const expected = ['this is wrong'];
-
-  const computed = en.clean(input);
-
-  t.deepEqual(computed, expected);
-});
-
-test('[EN] removes multiple commas at beginning of sentence with space', t => {
-  const input = [',,,,, this is wrong'];
-  const expected = ['this is wrong'];
-
-  const computed = en.clean(input);
-
-  t.deepEqual(computed, expected);
-});
-
-test('[EN] removes multiple commas at end of sentence', t => {
-  const input = ['this is wrong,,,,'];
-  const expected = ['this is wrong'];
-
-  const computed = en.clean(input);
-
-  t.deepEqual(computed, expected);
-});
+test('[EN] removes several spaces before exclamation mark', clean,
+  ['this is wrong ! And wrong too !'], ['this is wrong! And wrong too!']);
+test('[EN] removes multiple commas at beginning of sentence', clean, [',,,,this is wrong'], ['this is wrong']);
+test('[EN] removes multiple commas at beginning of sentence with space', clean, [',,,,, this is wrong'], ['this is wrong']);
+test('[EN] removes multiple commas at end of sentence', clean, ['this is wrong,,,,'], ['this is wrong']);
