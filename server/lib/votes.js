96394cf679eb6f405d2caa1cef1181886175f289
Michael Kohler <me@michaelkohler.info>
2020-05-14 00:11:04 +0200

    chore: make sure we do not allow multiple votes per user per sentence

--- server/lib/votes.js
+++ server/lib/votes.js
@@ -11,8 +11,19 @@ async function addVoteForSentence(voteParams) {
   debug('ADDING_VOTE_FOR_SENTENCE', voteParams.sentenceId);
 
   try {
-    await Vote.create(voteParams);
+    const [, created] = await Vote.findOrCreate({
+      where: {
+        sentenceId: voteParams.sentenceId,
+        user: voteParams.user,
+      },
+      defaults: voteParams,
+    });
+
+    if (!created) {
+      throw new Error('NOT_CREATED_ALREADY_EXISTING');
+    }
   } catch (error) {
     console.error('Failed adding vote for', voteParams.sentenceId);
+    throw error;
   }
 }
--- server/tests/lib/votes.test.js
+++ server/tests/lib/votes.test.js
@@ -5,7 +5,7 @@ import votes from '../../lib/votes';
 
 test.beforeEach((t) => {
   t.context.sandbox = sinon.createSandbox();
-  t.context.sandbox.stub(Vote, 'create').returns();
+  t.context.sandbox.stub(Vote, 'findOrCreate').resolves([undefined, true]);
 });
 
 test.afterEach.always((t) => {
@@ -21,11 +21,17 @@ test.serial('adds vote', async (t) => {
 
   await votes.addVoteForSentence(voteParams);
 
-  t.true(Vote.create.calledWith(voteParams));
+  t.true(Vote.findOrCreate.calledWith({
+    where: {
+      sentenceId: voteParams.sentenceId,
+      user: voteParams.user,
+    },
+    defaults: voteParams,
+  }));
 });
 
-test.serial('should catch error adding vote', async (t) => {
-  Vote.create.throws(new Error('nope!'));
+test.serial('should forward error adding vote', async (t) => {
+  Vote.findOrCreate.rejects(new Error('nope!'));
 
   const voteParams = {
     sentenceId: 1234,
@@ -33,5 +39,17 @@ test.serial('should catch error adding vote', async (t) => {
     approval: true,
   };
 
-  t.notThrows(() => votes.addVoteForSentence(voteParams));
+  await t.throwsAsync(() => votes.addVoteForSentence(voteParams));
+});
+
+test.serial('should not count vote if already existing vote', async (t) => {
+  Vote.findOrCreate.resolves([undefined, false]);
+
+  const voteParams = {
+    sentenceId: 1234,
+    user: 'foo',
+    approval: true,
+  };
+
+  await t.throwsAsync(() => votes.addVoteForSentence(voteParams));
 });
