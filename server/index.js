64627bb80ad9db27cb00f6c93d8cb67279a56c51
Michael Kohler <me@michaelkohler.info>
2020-05-29 18:24:03 +0200

    chore: increase timeout

--- server/index.js
+++ server/index.js
@@ -5,7 +5,8 @@ const app = require('./app');
 
 const port = process.env.PORT || '3333';
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   debug(`Listening on http://0.0.0.0:${port}..`);
 });
 
+server.setTimeout(300000);
