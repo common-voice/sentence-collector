2589d5f0c6c7e252adcc3cac06baefb0a0bf134f
Arthit Suriyawongkul <arthit@gmail.com>
2020-08-26 21:40:40 +0100

    Update regex and comments

--- shared/validation/languages/th.js
+++ shared/validation/languages/th.js
@@ -1,6 +1,10 @@
+// Notes
+// - Thai Unicode range: \u0E00-\u0E7F
+// - Thai sentence is written without space between words.
+
 // We count chars to validate instead of words.
 const MIN_LENGTH = 2;
-const MAX_LENGTH = 150;
+const MAX_LENGTH = 100;
 
 // Numbers that are not allowed in a sentence depending on the language. For
 // English this is 0-9 once or multiple times after each other.
@@ -9,18 +13,11 @@ const NUMBERS_REGEX = /[0-9๐-๙]+/;
 
 // Some languages want to check the structure, this is what this REGEX is for. For English
 // (and by extend as default) we're not currently using it.
-/* eslint-disable-next-line no-unused-vars */
-const STRUCTURE_REGEX = undefined;
-
-// The following symbols are disallowed, please update here as well and not just the regex
-// to make it easier to read:
-// < > + * \ # @ ^ [ ] ( ) /
 //
-// Latin characters with length of 3 or more are disallowed as well,
-// as they can be foriegn language.
+// Sentence with running characters of 70 or more without a space is considered difficult to read.
 //
 // These classes of Thai characters are not allowed to be immediately repeated:
-// * Lead vowels: \u0E40\u0E41\u0E42\u0E43\u0E44
+// - Lead vowels: \u0E40\u0E41\u0E42\u0E43\u0E44
 // - Follow vowels: \u0E30\u0E32\u0E33\u0E45
 // - Above vowels: \u0E31\u0E34\u0E35\u0E36\u0E37\u0E4D\u0E47
 // - Below vowels: \u0E38\u0E39
@@ -29,11 +26,24 @@ const STRUCTURE_REGEX = undefined;
 // - Thanthakhat: \u0E4C
 // - Nikhahit: \u0E4D
 // - Yamakkan: \u0E4E
-// The class with (*) symbol can't end the word.
-// The class with (-) symbol can't start the word.
+//
+// These classes of Thai characters have a specific legitimate order.
+// - Tone marks/Pinthu/Thanthakat/Nikhahit/Yamakkan can't immediately come after lead and follow vowels
+// - Tone marks/Pinthu/Thanthakat/Nikhahit/Yamakkan can't immediately come before above and below vowels
+/* eslint-disable-next-line no-unused-vars no-useless-escape */
+const STRUCTURE_REGEX= /[\u0E01-\u0E4Ea-zA-Z]{70,}|[\u0E40\u0E41\u0E42\u0E43\u0E44]{2,}|\u0E30{2,}|[\u0E32\u0E33\u0E45]{2,}|[\u0E31\u0E34\u0E35\u0E36\u0E37\u0E4D\u0E47]{2,}|[\u0E38\u0E39]{2,}|[\u0E48\u0E49\u0E4A\u0E4B]{2,}|\u0E3A{2,}|\u0E4C{2,}|\u0E4D{2,}|\u0E4E{2,}|[\u0E40\u0E41\u0E42\u0E43\u0E44\u0E30\u0E32\u0E33\u0E45][\u0E48\u0E49\u0E4A\u0E4B\u0E3A\u0E4C\u0E4D\u0E4E]|[\u0E48\u0E49\u0E4A\u0E4B\u0E3A\u0E4C\u0E4D\u0E4E][\u0E31\u0E34\u0E35\u0E36\u0E37\u0E4D\u0E47\u0E38\u0E39]/;
+
+// The following symbols are disallowed, please update here as well and not just the regex
+// to make it easier to read:
+// < > + * \ # @ ^ [ ] ( ) /
+// Fongman: \u0E4F
+// Angkhankhu: \u0E5A
+// Khomut: \u0E5B
+//
+// Latin characters with length of 3 or more are disallowed as well,
+// as they can be foriegn (non-Thai) language.
 /* eslint-disable-next-line no-useless-escape */
-// const SYMBOL_REGEX = /[<>\+\*\\#@\^\[\]\(\)\/]|[A-Za-z]{3,}|[\u0E40\u0E41\u0E42\u0E43\u0E44]{2,}|[\u0E30]{2,}|[\u0E32\u0E33\u0E45]{2,}|[\u0E31\u0E34\u0E35\u0E36\u0E37\u0E4D\u0E47]{2,}|[\u0E38\u0E39]{2,}|[\u0E48\u0E49\u0E4A\u0E4B]{2,}|[\u0E3A]{2,}|[\u0E4C]{2,}|[\u0E4D]{2,}|[\u0E4E]{2,}|(^|\W)[\u0E30\u0E32\u0E33\u0E45\u0E31\u0E34\u0E35\u0E36\u0E37\u0E4D\u0E47\u0E38\u0E39\u0E48\u0E49\u0E4A\u0E4B\u0E3A\u0E4C\u0E4D\u0E4E]|[\u0E40\u0E41\u0E42\u0E43\u0E44]($|\W)/u;
-const SYMBOL_REGEX = /[<>+*\\#@^[\]()/]|[A-Za-z]{3,}|[\u0E40\u0E41\u0E42\u0E43\u0E44]{2,}|\u0E30{2,}|[\u0E32\u0E33\u0E45]{2,}|[\u0E31\u0E34\u0E35\u0E36\u0E37\u0E4D\u0E47]{2,}|[\u0E38\u0E39]{2,}|[\u0E48\u0E49\u0E4A\u0E4B]{2,}|\u0E3A{2,}|\u0E4C{2,}|\u0E4D{2,}|\u0E4E{2,}/;
+const SYMBOL_REGEX = /[<>+*\\#@^[\]()/\u0E4F\u0E5A\u0E5B]|[A-Za-z]{3,}/
 
 // Any words consisting of uppercase letters or uppercase letters with a period
 // inbetween are considered abbreviations or acronyms.
@@ -53,9 +63,8 @@ export function filterSymbols(sentence) {
   return !sentence.match(SYMBOL_REGEX);
 }
 
-/* eslint-disable-next-line no-unused-vars */
 export function filterStructure(sentence) {
-  return true;
+  return !sentence.match(STRUCTURE_REGEX);
 }
 
 export function filterLength(sentence) {
