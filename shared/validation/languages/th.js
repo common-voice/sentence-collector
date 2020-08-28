99eb5eac601058afe2e2e3071173d25b8f1f8a93
Arthit Suriyawongkul <arthit@gmail.com>
2020-08-28 22:06:30 +0100

    Change from ^ and $ to \s

--- shared/validation/languages/th.js
+++ shared/validation/languages/th.js
@@ -38,19 +38,19 @@ const NUMBERS_REGEX = /[0-9๐-๙]+/;
 // Limit the use of Maiyamok (\u0E46 ๆ - repetition mark) to just one time per word,
 // to reduce disagreement during the record on how many times to actually voice out the sound.
 /* eslint-disable-next-line no-useless-escape */
-const STRUCTURE_REGEX = /[\u0E01-\u0E4Ea-zA-Z\.\,\-\"\'\?\!\:\;]{80,}|[\u0E40\u0E41\u0E42\u0E43\u0E44]{2,}|\u0E30{2,}|[\u0E32\u0E33\u0E45]{2,}|[\u0E31\u0E34\u0E35\u0E36\u0E37\u0E4D\u0E47]{2,}|[\u0E38\u0E39]{2,}|[\u0E48\u0E49\u0E4A\u0E4B]{2,}|\u0E3A{2,}|\u0E4C{2,}|\u0E4D{2,}|\u0E4E{2,}|[\u0E40\u0E41\u0E42\u0E43\u0E44\u0E30\u0E32\u0E33\u0E45][\u0E48\u0E49\u0E4A\u0E4B\u0E3A\u0E4C\u0E4D\u0E4E]|[\u0E48\u0E49\u0E4A\u0E4B\u0E3A\u0E4C\u0E4D\u0E4E][\u0E31\u0E34\u0E35\u0E36\u0E37\u0E4D\u0E47\u0E38\u0E39]|(\u0E46[ ]?){2,}/;
+const STRUCTURE_REGEX = /[\u0E01-\u0E4Ea-zA-Z\.\,\-\"\'\?\!\:\;]{80,}|[\u0E40\u0E41\u0E42\u0E43\u0E44]{2,}|\u0E30{2,}|[\u0E32\u0E33\u0E45]{2,}|[\u0E31\u0E34\u0E35\u0E36\u0E37\u0E4D\u0E47]{2,}|[\u0E38\u0E39]{2,}|[\u0E48\u0E49\u0E4A\u0E4B]{2,}|\u0E3A{2,}|\u0E4C{2,}|\u0E4D{2,}|\u0E4E{2,}|[\u0E40\u0E41\u0E42\u0E43\u0E44\u0E30\u0E32\u0E33\u0E45][\u0E48\u0E49\u0E4A\u0E4B\u0E3A\u0E4C\u0E4D\u0E4E]|[\u0E48\u0E49\u0E4A\u0E4B\u0E3A\u0E4C\u0E4D\u0E4E][\u0E31\u0E34\u0E35\u0E36\u0E37\u0E4D\u0E47\u0E38\u0E39]|(\u0E46[ ]*){2,}/;
 // These Thai chars cannot start the word:
 // - Lead vowels
 // - Tone marks
 // - Phinthu, Thanthakhat, Nikhahit, Yamakkan
 // - Maiyamok
 /* eslint-disable-next-line no-useless-escape */
-const BEGIN_REGEX = /^[\u0E30\u0E32\u0E33\u0E45\u0E31\u0E34\u0E35\u0E36\u0E37\u0E4D\u0E47\u0E38\u0E39\u0E48\u0E49\u0E4A\u0E4B\u0E3A\u0E4C\u0E4D\u0E4E\u0E46]/;
+const BEGIN_REGEX = /\s+[\u0E30\u0E32\u0E33\u0E45\u0E31\u0E34\u0E35\u0E36\u0E37\u0E4D\u0E47\u0E38\u0E39\u0E48\u0E49\u0E4A\u0E4B\u0E3A\u0E4C\u0E4D\u0E4E\u0E46]/;
 // These Thai chars cannot end the word:
 // - All vowels, except lead vowels
 // - Tone marks, except Mai Ek \u0E48
 /* eslint-disable-next-line no-useless-escape */
-const END_REGEX = /[\u0E40\u0E41\u0E42\u0E43\u0E44\u0E49\u0E4A\u0E4B]$/;
+const END_REGEX = /[\u0E40\u0E41\u0E42\u0E43\u0E44\u0E49\u0E4A\u0E4B]\s+/;
 
 // The following symbols are disallowed, please update here as well and not just the regex
 // to make it easier to read:
