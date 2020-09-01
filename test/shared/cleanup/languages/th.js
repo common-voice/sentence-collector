0adfae7d125813ec32a14a94c19d43ecf8508306
Arthit Suriyawongkul <arthit@gmail.com>
2020-09-01 21:41:08 +0100

    Update th.js
    Modified ! ? . test cases to include more variety of spaces

--- test/shared/cleanup/languages/th.js
+++ test/shared/cleanup/languages/th.js
@@ -23,13 +23,13 @@ test('removes several multiple spaces', clean,
   ['this is wrong on two levels', 'this is wrong on two levels'],
 );
 test('removes several spaces before colon', clean, ['this : is  : wrong'], ['this: is: wrong']);
-test('removes several comma', clean, ['this is, wrong , too'], ['this is wrong too']);
+test('removes several comma', clean, ['this,is, wrong , too, '], ['this is wrong too']);
 test('removes several dangling periods', clean,
   ['this is wrong . And wrong too .'], ['this is wrong And wrong too']);
 test('removes several question marks', clean,
-  ['this is wrong ? And wrong too ?'], ['this is wrong And wrong too']);
+  ['this is wrong ? And wrong too ? '], ['this is wrong And wrong too']);
 test('removes several exclamation marks', clean,
-  ['this is wrong ! And wrong too !'], ['this is wrong And wrong too']);
+  ['this is wrong !  And wrong too !'], ['this is wrong And wrong too']);
 test('removes multiple periods at beginning of sentence', clean, ['...this is wrong'], ['this is wrong']);
 test('removes multiple periods at beginning of sentence with space', clean, ['... this is wrong'], ['this is wrong']);
 test('removes multiple periods at end of sentence', clean, ['this is wrong...'], ['this is wrong']);
