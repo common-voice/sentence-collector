b9e39654dd7e43ba78525bacd0da61a93be89b8d
Arthit Suriyawongkul <arthit@gmail.com>
2020-09-05 20:41:57 +0100

    Update rules. No longer remove ? and !

--- shared/cleanup/languages/th.js
+++ shared/cleanup/languages/th.js
@@ -2,24 +2,32 @@ export function sortSentences(sentences) {
   return sentences.sort();
 }
 
+// Guidelines from the Office of the Royal Society
+// comma http://www.royin.go.th/?page_id=10392
+//   (in bibliography/index use case, removing comma may change the sentence's semantic,
+//   but that use case seems irrelevant here and in any case does not affect sound)
+// colon http://www.royin.go.th/?page_id=10399
+// question mark http://www.royin.go.th/?page_id=10418
+// exclamation mark http://www.royin.go.th/?page_id=10433
+// Maiyamok http://www.royin.go.th/?page_id=10427
 export function clean(sentences) {
   return sentences.map((sentence) => {
     return sentence
       .replace(/[\u200b\u200c]/g, '')  // removes zero-width chars (occurs in some Thai texts)
-      .replace(/\?/g, ' ') // Thai does not use question mark
-      .replace(/!/g, ' ') // Thai does not use exclamation mark
-      .replace(/,/g, ' ') // Thai does not use comma
+      .replace(/,/g, ' ') // replaces comma with space
+      .replace(/\s:/g, ' : ') // add a space before and after colon
+      .replace(/\?/g, ' ? ') // adds a space before and after question mark
+      .replace(/!/g, ' ! ') // adds a space before and after exclamation mark
       .replace(/(\u0E46\s*)+/g, '\u0E46') // condenses multiple Maiyamok to one Maiyamok
       .replace(/\u0E46/g, ' \u0E46 ') // adds a space before and after Maiyamok
-      .replace(/\u0E40\u0E40/g, '\u0E41') // normalize Sara E + Sara E -> Sara Ae
-      .replace(/\u0E4d([\u0E48\u0E49\u0E4A\u0E4B]*)\u0E32/g, '$1\u0e33') // normalize Nikhahit + Sara Aa -> Sara Am
+      .replace(/\u0E40\u0E40/g, '\u0E41') // normalizes Sara E + Sara E -> Sara Ae
+      .replace(/\u0E4d([\u0E48\u0E49\u0E4A\u0E4B]*)\u0E32/g, '$1\u0e33') // normalizes Nikhahit + Sara Aa -> Sara Am
       .replace(/\s+/g, ' ') // condenses multiple spaces to one space
-      .replace(/\.(\.\s*)+/g, '') // Thai does not use ellipsis (.., ...)
-      .replace(/\s\./g, '') // removes dangling period
-      .replace(/\s:/g, ':')
-      .replace(/^\./, '') // we do not want periods at the beginning of the sentence
-      .replace(/^\s+/, '') // we do not want spaces at the beginning of the sentence
-      .replace(/\s+$/, '') // we do not want spaces at the end of the sentence
+      .replace(/\.(\.\s*)+/g, '') // removes ellipsis (.., ...)
+      .replace(/\s\./g, '') // removes orphan period
+      .replace(/^\./, '') // removes periods at the beginning of the sentence
+      .replace(/^\s+/, '') // removes spaces at the beginning of the sentence
+      .replace(/\s+$/, '') // removes spaces at the end of the sentence
       ;
   });
 }
--- test/shared/cleanup/languages/th.js
+++ test/shared/cleanup/languages/th.js
@@ -9,10 +9,10 @@ function clean(t, input, expected) {
 test('condenses multiple Maiyamok', clean, ['this is wrong ๆๆ ๆ   ๆ'], ['this is wrong ๆ']);
 test('add a space before and after Maiyamok', clean, ['this isๆ wrong'], ['this is ๆ wrong']);
 test('removes multiple spaces', clean, ['this is  wrong'], ['this is wrong']);
-test('removes space before colon', clean, ['this is : wrong'], ['this is: wrong']);
+test('removes space before colon', clean, ['this is:  wrong'], ['this is : wrong']);
 test('removes comma', clean, ['this is , wrong'], ['this is wrong']);
-test('removes exclamation mark', clean, ['this is wrong!'], ['this is wrong']);
-test('removes question mark', clean, ['this is wrong?'], ['this is wrong']);
+test('removes exclamation mark', clean, ['this is wrong!'], ['this is wrong !']);
+test('removes question mark', clean, ['this is wrong?'], ['this is wrong ?']);
 test('removes ellipsis', clean, ['this is wrong...'], ['this is wrong']);
 test('removes several ellipsis', clean, ['this ... is wrong . . . ... '], ['this is wrong']);
 test('removes dangling period', clean, ['this is wrong .'], ['this is wrong']);
@@ -22,17 +22,16 @@ test('removes several multiple spaces', clean,
   ['this is  wrong  on two levels', 'this is  wrong       on two levels'],
   ['this is wrong on two levels', 'this is wrong on two levels'],
 );
-test('removes several spaces before colon', clean, ['this : is  : wrong'], ['this: is: wrong']);
+test('removes several spaces before colon', clean, ['this : is  : wrong'], ['this : is : wrong']);
 test('removes several comma', clean, ['this,is, wrong , too, '], ['this is wrong too']);
 test('removes several dangling periods', clean,
-  ['this is wrong . And wrong too .'], ['this is wrong And wrong too']);
+  ['this is wrong .  And wrong too .'], ['this is wrong And wrong too']);
 test('removes several question marks', clean,
-  ['this is wrong ? And wrong too ? '], ['this is wrong And wrong too']);
+  ['this is wrong ? And wrong too ? '], ['this is wrong ? And wrong too ?']);
 test('removes several exclamation marks', clean,
-  ['this is wrong !  And wrong too !'], ['this is wrong And wrong too']);
+  ['this is wrong !  And wrong too !'], ['this is wrong ! And wrong too !']);
 test('removes multiple periods at beginning of sentence', clean, ['...this is wrong'], ['this is wrong']);
 test('removes multiple periods at beginning of sentence with space', clean, ['... this is wrong'], ['this is wrong']);
 test('removes multiple periods at end of sentence', clean, ['this is wrong...'], ['this is wrong']);
 test('normalize Sara E Sara E', clean, ['เรือนเเพ'], ['เรือนแพ']);
 test('normalize Sara Am', clean, ['ลํานํ้า'], ['ลำน้ำ']);
-
