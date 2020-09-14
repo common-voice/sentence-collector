7fb334f769389891c7afd3052c80d7cd6a8fe914
Arthit Suriyawongkul <arthit@gmail.com>
2020-09-14 17:08:01 +0100

    Add cleanup for Thai (#325)
    * Add cleanup for Thai
    
    * Fix test: Error: Duplicate test title: removes ellipsis
    
    * Update rules and tests for periods
    
    * Add tests for normalize
    
    * Update th.js
    
    Modified ! ? . test cases to include more variety of spaces
    
    * Update rules. No longer remove ? and !
    
    * Fix colon rule
    
    * normalizes Ru/Lu + Sara Aa -> Ru/Lu + Lakkhangyao
    
    * Missing semicolon
    
    * Fix test. Update CLEANUP.md
    
    * Update test case names

---
+++ shared/cleanup/languages/th.js
@@ -0,0 +1,34 @@
+export function sortSentences(sentences) {
+  return sentences.sort();
+}
+
+// Guidelines from the Office of the Royal Society
+// comma http://www.royin.go.th/?page_id=10392
+//   (in bibliography/index use case, removing comma may change the sentence's semantic,
+//   but that use case seems irrelevant here and in any case does not affect sound)
+// colon http://www.royin.go.th/?page_id=10399
+// question mark http://www.royin.go.th/?page_id=10418
+// exclamation mark http://www.royin.go.th/?page_id=10433
+// Maiyamok http://www.royin.go.th/?page_id=10427
+export function clean(sentences) {
+  return sentences.map((sentence) => {
+    return sentence
+      .replace(/[\u200b\u200c]/g, '')  // removes zero-width chars (occurs in some Thai texts)
+      .replace(/:/g, ' : ') // add a space before and after colon
+      .replace(/\?/g, ' ? ') // adds a space before and after question mark
+      .replace(/!/g, ' ! ') // adds a space before and after exclamation mark
+      .replace(/,/g, ' ') // replaces comma with space
+      .replace(/\.(\.\s*)+/g, ' ') // replaces ellipsis (.., ...) with space
+      .replace(/\s\./g, ' ') // replaces orphan period with space
+      .replace(/(\u0E46\s*)+/g, '\u0E46') // condenses multiple Maiyamok to one Maiyamok
+      .replace(/\u0E46/g, ' \u0E46 ') // adds a space before and after Maiyamok
+      .replace(/\s+/g, ' ') // condenses multiple spaces to one space
+      .replace(/^\./, '') // removes periods at the beginning of the sentence
+      .replace(/^\s+/, '') // removes spaces at the beginning of the sentence
+      .replace(/\s+$/, '') // removes spaces at the end of the sentence
+      .replace(/\u0E40\u0E40/g, '\u0E41') // normalizes Sara E + Sara E -> Sara Ae
+      .replace(/\u0E4d([\u0E48\u0E49\u0E4A\u0E4B]*)\u0E32/g, '$1\u0E33') // normalizes Nikhahit + Sara Aa -> Sara Am
+      .replace(/([\u0E24\u0E26])\u0E32/g, '$1\u0E45') // normalizes Ru/Lu + Sara Aa -> Ru/Lu + Lakkhangyao
+      ;
+  });
+}
