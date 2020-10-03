2acec2c57e369666b0160740245ffbc86376295e
Michael Kohler <me@michaelkohler.info>
2020-10-04 00:33:39 +0200

    chore: use function and hooks for confirm/submit forms

--- web/src/components/confirm-form.js
+++ web/src/components/confirm-form.js
@@ -1,91 +1,67 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import ReviewLink from './review-link';
 import SpinnerButton from './spinner-button';
 
-class ConfirmForm extends React.Component {
-  constructor(props) {
-    super(props);
+export default function ConfirmForm(props) {
+  const {
+    submitted,
+    invalidated,
+    validated,
+    unreviewed,
+    onSubmit,
+    onReview,
+  } = props;
+  const { isUploadingSentences } = useSelector((state) => state.sentences);
 
-    this.state = {};
-    this.onSubmit = this.onSubmit.bind(this);
-    this.onReview = this.props.onReview.bind(this);
-  }
+  const readyCount = submitted.length - invalidated.length;
 
-  onSubmit(evt) {
-    this.setState({
-      pendingSentences: true,
-    });
+  return (
+    <form onSubmit={onSubmit}>
+      <h2>Confirm New Sentences</h2>
+      <p>
+        {`${submitted.length} sentences found.`}
+      </p>
 
-    this.props.onSubmit(evt);
-  }
-
-  render() {
-    const {
-      submitted,
-      invalidated,
-      validated,
-      unreviewed,
-      isUploadingSentences,
-    } = this.props;
-
-    const readyCount = submitted.length - invalidated.length;
-
-    return (
-      <form onSubmit={this.onSubmit}>
-        <h2>Confirm New Sentences</h2>
-        <p>
-          {`${submitted.length} sentences found.`}
+      {invalidated.length > 0 && (
+        <p style={{color: 'red'}}>
+          {`${invalidated.length} rejected by you`}
         </p>
+      )}
 
-        {invalidated.length > 0 && (
-          <p style={{color: 'red'}}>
-            {`${invalidated.length} rejected by you`}
-          </p>
+      {validated.length + invalidated.length > 0 && (
+        <p>
+          {`-- ${validated.length + invalidated.length}`} {}
+          sentences are already reviewed. Great job!
+        </p>
+      )}
+
+      <p><strong>{`${readyCount} sentences ready for submission!`}</strong></p>
+
+      {unreviewed.length > 0 && (
+        <p>
+          {`-- ${unreviewed.length} of these sentences are unreviewed. If you want, you can also review your sentences now before submitting them.`}&nbsp;
+          <ReviewLink onReview={onReview}
+                      sentences={unreviewed} />
+        </p>
+      )}
+
+      <section id="confirm-buttons">
+        { isUploadingSentences ?
+          <SpinnerButton></SpinnerButton> :
+          <button type="submit" disabled={readyCount === 0}>Confirm</button>
+        }
+
+        { isUploadingSentences && (
+          <div>
+            <p className="loadingText">
+              Sentences are being uploaded. This can take several minutes depending on the number of sentences added.
+              Please do not close this website.
+            </p>
+          </div>
         )}
-
-        {validated.length + invalidated.length > 0 && (
-          <p>
-            {`-- ${validated.length + invalidated.length}`} {}
-            sentences are already reviewed. Great job!
-          </p>
-        )}
-
-        <p><strong>{`${readyCount} sentences ready for submission!`}</strong></p>
-
-        {unreviewed.length > 0 && (
-          <p>
-            {`-- ${unreviewed.length} of these sentences are unreviewed. If you want, you can also review your sentences now before submitting them.`}&nbsp;
-            <ReviewLink onReview={this.onReview}
-                        sentences={unreviewed} />
-          </p>
-        )}
-
-        <section id="confirm-buttons">
-          { isUploadingSentences ?
-            <SpinnerButton></SpinnerButton> :
-            <button type="submit" disabled={readyCount === 0}>Confirm</button>
-          }
-
-          { isUploadingSentences && (
-            <div>
-              <p className="loadingText">
-                Sentences are being uploaded. This can take several minutes depending on the number of sentences added.
-                Please do not close this website.
-              </p>
-            </div>
-          )}
-        </section>
-      </form>
-    );
-  }
+      </section>
+    </form>
+  );
 }
-
-function mapStateToProps(state) {
-  return {
-    isUploadingSentences: state.sentences.isUploadingSentences,
-  };
-}
-
-export default connect(mapStateToProps)(ConfirmForm);
--- web/src/components/submit-form.js
+++ web/src/components/submit-form.js
@@ -1,104 +1,82 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import LanguageSelector from './language-selector';
 
-class SubmitForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.onSubmit = this.onSubmit.bind(this);
+export default function SubmitForm({ onSubmit, message, error }) {
+  const {
+    allLanguages,
+    languages,
+  } = useSelector((state) => state.languages);
+  const {
+    sentenceSubmissionFailures,
+  } = useSelector((state) => state.sentences);
+
+  let extendedLanguages = languages.map((lang) => allLanguages.find((extendedLanguage) => extendedLanguage.id === lang)).filter(Boolean);
+  if (extendedLanguages.length < 1) {
+    extendedLanguages = allLanguages;
   }
 
-  onSubmit(evt) {
-    this.props.onSubmit(evt);
-  }
+  return (
+    <React.Fragment>
+      <form id="add-form" onSubmit={onSubmit}>
+        <h2>Add Sentences</h2>
 
-  render() {
-    const {
-      message,
-      error,
-      languages,
-      allLanguages,
-      sentenceSubmissionFailures,
-    } = this.props;
+        { message && (<section className="form-message">{ message }</section>)}
+        { error && (<section className="form-error">{ error }</section>)}
 
-    let extendedLanguages = languages.map((lang) => allLanguages.find((extendedLanguage) => extendedLanguage.id === lang)).filter(Boolean);
-    if (extendedLanguages.length < 1) {
-      extendedLanguages = allLanguages;
-    }
+        <section>
+          <label className="language-selector-label" htmlFor="language-selector">
+            Select Language
+          </label>
+          <LanguageSelector name="language-selector" languages={extendedLanguages}/>
+        </section>
+        <section>
+          <label htmlFor="sentences-input">
+            Add <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Public_domain">public domain</a> sentences
+          </label>
+          <textarea id="sentences-input" placeholder="One sentence per line" />
+        </section>
+        <section>
+          <label htmlFor="source-input">
+            Where are these <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Public_domain">public domain</a> sentences from?
+          </label>
+          <input id="source-input" type="text" placeholder="Read our How-to if unsure how to attribute" />
+        </section>
+        <section>
+          <input id="agree" type="checkbox" />
+          <label htmlFor="agree">
+            I confirm that these sentences are {}
+            <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Public_domain">public domain</a> {}
+            and I have permission to upload them.
+          </label>
+        </section>
 
-    return (
-      <React.Fragment>
-        <form id="add-form" onSubmit={this.onSubmit}>
-          <h2>Add Sentences</h2>
+        <section id="confirm-buttons" className="divCenter">
+          <button>Submit</button>
+        </section>
+      </form>
 
-          { message && (<section className="form-message">{ message }</section>)}
-          { error && (<section className="form-error">{ error }</section>)}
+      { Object.keys(sentenceSubmissionFailures).length > 0 && (
+        <section>
+          <h2>Filtered sentences due to requirements failing (please submit fixed versions as new sentences):</h2>
+          <p>Please check the <Link to={'/how-to'}>guidelines</Link>.</p>
 
-          <section>
-            <label className="language-selector-label" htmlFor="language-selector">
-              Select Language
-            </label>
-            <LanguageSelector name="language-selector" languages={extendedLanguages}/>
-          </section>
-          <section>
-            <label htmlFor="sentences-input">
-              Add <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Public_domain">public domain</a> sentences
-            </label>
-            <textarea id="sentences-input" placeholder="One sentence per line" />
-          </section>
-          <section>
-            <label htmlFor="source-input">
-              Where are these <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Public_domain">public domain</a> sentences from?
-            </label>
-            <input id="source-input" type="text" placeholder="Read our How-to if unsure how to attribute" />
-          </section>
-          <section>
-            <input id="agree" type="checkbox" />
-            <label htmlFor="agree">
-              I confirm that these sentences are {}
-              <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Public_domain">public domain</a> {}
-              and I have permission to upload them.
-            </label>
-          </section>
-
-          <section id="confirm-buttons" className="divCenter">
-            <button>Submit</button>
-          </section>
-        </form>
-
-        { Object.keys(sentenceSubmissionFailures).length > 0 && (
-          <section>
-            <h2>Filtered sentences due to requirements failing (please submit fixed versions as new sentences):</h2>
-            <p>Please check the <Link to={'/how-to'}>guidelines</Link>.</p>
-
-            {
-              Object.keys(sentenceSubmissionFailures).map((filterKey) => (
-                <React.Fragment key={'fragment-' + filterKey}>
-                  <h3 key="{filterKey}">{ filterKey }</h3>
-                  {
-                    sentenceSubmissionFailures[filterKey].map((filteredSentence) =>
-                      <p key={filteredSentence}>{filteredSentence}</p>
-                    )
-                  }
-                </React.Fragment>
-              ))
-            }
-          </section>
-        )}
-      </React.Fragment>
-    );
-  }
+          {
+            Object.keys(sentenceSubmissionFailures).map((filterKey) => (
+              <React.Fragment key={'fragment-' + filterKey}>
+                <h3 key="{filterKey}">{ filterKey }</h3>
+                {
+                  sentenceSubmissionFailures[filterKey].map((filteredSentence) =>
+                    <p key={filteredSentence}>{filteredSentence}</p>
+                  )
+                }
+              </React.Fragment>
+            ))
+          }
+        </section>
+      )}
+    </React.Fragment>
+  );
 }
-
-function mapStateToProps(state) {
-  return {
-    allLanguages: state.languages.allLanguages,
-    languages: state.languages.languages,
-    errorMessage: state.sentences.errorMessage,
-    sentenceSubmissionFailures: state.sentences.sentenceSubmissionFailures,
-  };
-}
-
-export default connect(mapStateToProps)(SubmitForm);
