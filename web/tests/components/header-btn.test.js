e45a352cc4523d83a23c1000bead2bd68ba9030e
Michael Kohler <me@michaelkohler.info>
2020-10-04 23:16:23 +0200

    chore: add initial test

---
+++ web/tests/components/header-btn.test.js
@@ -0,0 +1,18 @@
+import { mount } from 'enzyme';
+import React from 'react';
+
+import HeaderBtn from '../../src/components/header-btn';
+
+const clickHandler = jest.fn();
+
+test('Header button should be rendered correctly when closed', () => {
+  let component = mount(<HeaderBtn isOpen={false} handleClick={clickHandler} />);
+
+  expect(component).toMatchSnapshot();
+});
+
+test('Header button should be rendered correctly when open', () => {
+  let component = mount(<HeaderBtn isOpen={true} handleClick={clickHandler} />);
+
+  expect(component).toMatchSnapshot();
+});
