e32ca0e498b27a04bfefbf2afdde6e76c71ea1b3
Jindřich Dítě <adrijaned@gmail.com>
2020-11-11 20:20:25 +0100

    fix: add migration to remove hi, ja copyrighted sentences (#389)
    * fix: add migration to remove hi, ja copyrighted sentences
    
    * fix: fix fix for removing hi and ja copyrighted sentences

---
+++ server/migrations/20201111000000-remove-hi-ja-sentences.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface) => {
+    return  queryInterface.sequelize.query(`
+      DELETE FROM Sentences
+        WHERE (localeId="hi" AND source="https://tatoeba.org/eng")
+	  OR (localeId="ja" AND (
+            source="http://d.hatena.ne.jp/satoru_net/20151030/1446184756" OR
+	    source="https://github.com/voice-statistics/voice-statistics.github.com/blob/master/assets/doc/balance_sentences.txt" OR
+	    source="https://github.com/matbahasa/TALPCo/blob/master/data_jpn.txt"
+	  ))
+    `);
+  },
+  down: () => Promise.resolve(),
+};
