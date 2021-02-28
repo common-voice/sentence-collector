b86cd4f8d8ce41660a14287693fd74aaf6b5ed58
Michael Kohler <me@michaelkohler.info>
2021-02-28 15:08:22 +0100

    chore: add tests for all reducers

--- web/src/reducers/languages.test.js
+++ web/src/reducers/languages.test.js
@@ -18,6 +18,34 @@ test('should use initial state', async () => {
   });
 });
 
+test('should reduce getting stats', async () => {
+  const newState = languageReducer({}, {
+    type: languages.ACTION_GET_STATS,
+  });
+
+  expect(newState.statsUpdating).toEqual(true);
+});
+
+test('should reduce stats', async () => {
+  const stats = [{ foo: 'bar' }];
+  const newState = languageReducer({}, {
+    type: languages.ACTION_GOT_STATS,
+    stats,
+  });
+
+  expect(newState.stats).toEqual(stats);
+  expect(newState.lastStatsUpdate).not.toBe(null);
+  expect(newState.statsUpdating).toEqual(false);
+});
+
+test('should reduce stats status reset', async () => {
+  const newState = languageReducer({ statsUpdating: true }, {
+    type: languages.ACTION_RESET_STATS_STATUS,
+  });
+
+  expect(newState.statsUpdating).toEqual(false);
+});
+
 test('should reduce languages', async () => {
   const newState = languageReducer({}, {
     type: languages.ACTION_GOT_LANGUAGES,
@@ -25,4 +53,56 @@ test('should reduce languages', async () => {
   });
 
   expect(newState.allLanguages).toEqual(mockLanguages);
+});
+
+test('should reduce add language request', async () => {
+  const newState = languageReducer({}, {
+    type: languages.ACTION_ADD_LANGUAGE_REQUEST,
+  });
+
+  expect(newState.pendingLanguages).toEqual(true);
+});
+
+test('should reduce add languages', async () => {
+  const newState = languageReducer({ pendingLanguages: true }, {
+    type: languages.ACTION_ADD_LANGUAGE_SUCCESS,
+    languages: mockLanguages,
+  });
+
+  expect(newState.languages).toEqual(mockLanguages);
+  expect(newState.pendingLanguages).toEqual(false);
+});
+
+test('should reduce add language failure', async () => {
+  const newState = languageReducer({ pendingLanguages: true }, {
+    type: languages.ACTION_ADD_LANGUAGE_FAILURE,
+  });
+
+  expect(newState.pendingLanguages).toEqual(false);
+});
+
+test('should reduce remove language request', async () => {
+  const newState = languageReducer({}, {
+    type: languages.ACTION_REMOVE_LANGUAGE_REQUEST,
+  });
+
+  expect(newState.pendingLanguages).toEqual(true);
+});
+
+test('should reduce remove languages', async () => {
+  const newState = languageReducer({ pendingLanguages: true }, {
+    type: languages.ACTION_REMOVE_LANGUAGE_SUCCESS,
+    languages: mockLanguages,
+  });
+
+  expect(newState.languages).toEqual(mockLanguages);
+  expect(newState.pendingLanguages).toEqual(false);
+});
+
+test('should reduce remove language failure', async () => {
+  const newState = languageReducer({ pendingLanguages: true }, {
+    type: languages.ACTION_REMOVE_LANGUAGE_FAILURE,
+  });
+
+  expect(newState.pendingLanguages).toEqual(false);
 });
---
+++ web/src/reducers/sentences.test.js
@@ -0,0 +1,139 @@
+import * as sentences from '../actions/sentences';
+import sentencesReducer from './sentences';
+
+test('should use initial state', async () => {
+  const newState = sentencesReducer(undefined, {
+    type: 'inexistant',
+  });
+
+  expect(newState).toEqual({
+    sentenceSubmissionFailures: [],
+    isUploadingSentences: false,
+    rejectedSentencesLoading: false,
+    rejectedSentences: [],
+    rejectedSentencesError: null,
+    sentences: [],
+    sentencesLoading: false,
+    reviewMessage: null,
+  });
+});
+
+test('should reduce submit request', async () => {
+  const newState = sentencesReducer({ sentenceSubmissionFailures: ['oh no'] }, {
+    type: sentences.ACTION_SUBMIT_SENTENCES_REQUEST,
+  });
+
+  expect(newState.sentenceSubmissionFailures).toEqual([]);
+  expect(newState.isUploadingSentences).toEqual(true);
+});
+
+test('should reduce submission failure', async () => {
+  const submissionFailures = [{
+    error: 'Too long',
+    sentence: 'Super super long sentence',
+  }, {
+    error: 'Too long',
+    sentence: 'Another super long sentence',
+  }, {
+    error: 'Has symbols',
+    sentence: '$$$$$',
+  }];
+  const newState = sentencesReducer({}, {
+    type: sentences.ACTION_SUBMIT_SENTENCES_FAILURE,
+    errors: submissionFailures,
+  });
+
+  expect(newState.sentenceSubmissionFailures).toEqual({
+    'Too long': [
+      'Super super long sentence',
+      'Another super long sentence',
+    ],
+    'Has symbols': ['$$$$$'],
+  });
+});
+
+test('should reduce submit done', async () => {
+  const newState = sentencesReducer({ isUploadingSentences: true }, {
+    type: sentences.ACTION_SUBMIT_SENTENCES_DONE,
+  });
+
+  expect(newState.isUploadingSentences).toEqual(false);
+});
+
+test('should reduce rejected sentences request', async () => {
+  const newState = sentencesReducer({ rejectedSentencesError: 'oh no' }, {
+    type: sentences.ACTION_LOAD_REJECTED_SENTENCES,
+  });
+
+  expect(newState.rejectedSentencesLoading).toEqual(true);
+  expect(newState.rejectedSentencesError).toEqual(null);
+});
+
+test('should reduce rejected sentences', async () => {
+  const testSentences = ['Hi', 'All good?'];
+  const newState = sentencesReducer({}, {
+    type: sentences.ACTION_GOT_REJECTED_SENTENCES,
+    sentences: testSentences,
+  });
+
+  expect(newState.rejectedSentencesLoading).toEqual(false);
+  expect(newState.rejectedSentences).toEqual(testSentences);
+});
+
+test('should reduce rejected sentences failure', async () => {
+  const errorMessage = 'oh no';
+  const newState = sentencesReducer({ rejectedSentencesLoading: true }, {
+    type: sentences.ACTION_REJECTED_SENTENCES_FAILURE,
+    errorMessage,
+  });
+
+  expect(newState.rejectedSentencesLoading).toEqual(false);
+  expect(newState.rejectedSentences).toEqual([]);
+  expect(newState.rejectedSentencesError).toEqual(errorMessage);
+});
+
+test('should reduce loading sentences', async () => {
+  const newState = sentencesReducer({}, {
+    type: sentences.ACTION_LOAD_SENTENCES,
+  });
+
+  expect(newState.sentencesLoading).toEqual(true);
+});
+
+test('should reduce sentences', async () => {
+  const testSentences = ['Hi', 'All good?'];
+  const newState = sentencesReducer({ sentencesLoading: true }, {
+    type: sentences.ACTION_GOT_SENTENCES,
+    sentences: testSentences,
+  });
+
+  expect(newState.sentencesLoading).toEqual(false);
+  expect(newState.sentences).toEqual(testSentences);
+});
+
+test('should reduce reviewed sentences', async () => {
+  const newState = sentencesReducer({}, {
+    type: sentences.ACTION_REVIEWED_SENTENCES,
+    votes: 10,
+  });
+
+  expect(newState.reviewMessage).toEqual('10 sentences reviewed. Thank you!');
+});
+
+test('should reduce reviewed sentences failure', async () => {
+  const errorMessage = 'oh no';
+  const newState = sentencesReducer({}, {
+    type: sentences.ACTION_REVIEW_SENTENCES_FAILURE,
+    errorMessage,
+  });
+
+  expect(newState.reviewMessage).toEqual(errorMessage);
+});
+
+test('should reduce review message reset', async () => {
+  const newState = sentencesReducer({ reviewMessage: 'hi' }, {
+    type: sentences.ACTION_REVIEW_RESET_MESSAGE,
+  });
+
+  expect(newState.reviewMessage).toEqual('');
+});
