673e0f619e5c402f4dc87b0c2f1173167b66fd4c
Michael Kohler <me@michaelkohler.info>
2021-03-07 18:46:48 +0100

    fix: add contribution tiles on front page and update text

--- web/src/containers/home.test.js
+++ web/src/containers/home.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 
 import Home from './home';
 
 test('should render home', () => {
-  const { container } = render(<Home/>);
+  const { container } = render(<BrowserRouter><Home/></BrowserRouter>);
   expect(container).toMatchSnapshot();
 });
