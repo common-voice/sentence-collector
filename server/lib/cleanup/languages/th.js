1e3aad4331016dc6de24b6b8a2735d6f2295aba3
Arthit Suriyawongkul <arthit@gmail.com>
2021-04-25 17:29:11 +0700

    Deal with U+2063 (invisible separator) which occurs in Thai text cut & pasted from some text editors (like MS Word and iOS Notes)

--- server/lib/cleanup/languages/th.js
+++ server/lib/cleanup/languages/th.js
@@ -7,7 +7,11 @@ function sortSentences(sentences) {
   return sentences.sort();
 }
 
-// Guidelines from the Office of the Royal Society
+// Remove zero-width chars and invisible separators
+// that may occur in copy-and-pasted text
+//
+// Normalize Thai punctuation using guidelines
+// from the Office of the Royal Society:
 // comma http://www.royin.go.th/?page_id=10392
 //   (in bibliography/index use case, removing comma may change the sentence's semantic,
 //   but that use case seems irrelevant here and in any case does not affect sound)
@@ -16,13 +20,13 @@ function sortSentences(sentences) {
 // exclamation mark http://www.royin.go.th/?page_id=10433
 // Maiyamok http://www.royin.go.th/?page_id=10427
 //
-// Emoji range from
+// Remove emojis, using emoji range from:
 // https://www.regextester.com/106421
 // https://stackoverflow.com/questions/10992921/how-to-remove-emoji-code-using-javascript
 function clean(sentences) {
   return sentences.map((sentence) => {
     return sentence
-      .replace(/[\u200b\u200c]/g, '')  // remove zero-width chars (occurs in some Thai texts)
+      .replace(/[\u200b\u200c\u2063]/g, '')  // remove zero-width chars (occurs in some Thai texts)
       .replace(/\u00a9|\u00ae|[\u2000-\u3300]|[\u2580-\u27bf]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]|[\ue000-\uf8ff]/g, '')  // remove emoji
       .replace(/:/g, ' : ')  // add a space before and after colon
       .replace(/\?/g, ' ? ')  // add a space before and after question mark
