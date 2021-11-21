c6a19b6bb051d42a25aa9a5a8fb5f85fa04e5895
Michael Kohler <me@michaelkohler.info>
2021-11-21 18:45:56 +0100

    chore: also export SC strings to CV file before moving it back

--- scripts/pull-common-voice-translations.sh
+++ scripts/pull-common-voice-translations.sh
@@ -4,6 +4,7 @@ HERE=$(dirname $0)
 
 echo "Pulling translations from Common Voice..."
 git clone --depth=1 https://github.com/common-voice/common-voice.git $HERE/cv
+env COMMON_VOICE_PATH=cv/ node $HERE/export-source-strings.js
 cp -r $HERE/cv/web/locales $HERE/../web/
 
 echo "Removing temporary Common Voice repo..."
