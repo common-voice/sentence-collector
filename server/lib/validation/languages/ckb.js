e9dd679fb2eba4d4821f96fce02a886f95b35755
Michael Kohler <me@michaelkohler.info>
2021-12-07 18:37:51 +0100

    fix: do not allow % and add translations

--- server/lib/validation/languages/ckb.js
+++ server/lib/validation/languages/ckb.js
@@ -12,16 +12,16 @@ const INVALIDATIONS = [{
     const words = tokenizeWords(sentence);
     return words.length < MIN_WORDS || words.length > MAX_WORDS;
   },
-  error: `Number of words must be between 1 and 14 (inclusive)`,
+  error: 'ژمارەی وشەکان دەبێت لەنێوان ١ بۆ ١٤ بێت',
 }, {
   regex: /[0-9\u06f0\u06f1\u06f2\u06f3\u06f4\u06f5\u06f6\u06f7\u06f8\u06f9]+/,
-  error: 'Sentence should not contain numbers',
+  error: 'نابێت ڕستە ژمارەى تێدا بێت',
 }, {
-  regex: /[<>+*#@^[\]()/]/,
-  error: 'Sentence should not contain symbols',
+  regex: /[<>+*#@%^[\]()/]/,
+  error: 'نابێت ڕستە هێمای تێدا بێت',
 }, {
   regex: /[\u0649\u0643\u0622\u0625\u0623\u0629\u0624\u064a\u200c\u06c0\u062b\u0630\u0638\u0637\u0636\u0635]+/,
-  error: 'Sentence should not contain non-Central-Kurdish characters',
+  error: 'نابێت ڕستە پیتی بەدەر لە ئەلفوبێی کوردی (سۆرانی) تێدا بێت',
 }];
 
 module.exports = {
