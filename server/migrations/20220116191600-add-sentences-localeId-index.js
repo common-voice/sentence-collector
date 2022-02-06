0d5806e4b318122ecbcb48551bd4b2fba38bd37d
Michael Kohler <me@michaelkohler.info>
2022-02-06 13:03:37 +0100

    fix: fix index creation

--- server/migrations/20220116191600-add-sentences-localeId-index.js
+++ server/migrations/20220116191600-add-sentences-localeId-index.js
@@ -1,13 +1,24 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface) => queryInterface.addIndex(
-    'Sentences',
-    ['localeId'],
-    {
-      indexName: 'SentencesLocaleIdIndex',
-      indicesType: 'FULLTEXT',
+  up: async (queryInterface) => {
+    // The localeId on the Sentences table already seems to exist on staging and production, so
+    // we need to make sure we do not add a duplicate index. Therefore we remove the index
+    // if it exists and then re-add it.
+    const indexes = await queryInterface.showIndex('Sentences');
+    const localeIdIndex = indexes.find((index) => index.name === 'sentences_locale_id');
+    if (localeIdIndex) {
+      await queryInterface.removeIndex('Sentences', 'sentences_locale_id');
     }
-  ),
-  down: (queryInterface) => queryInterface.removeIndex('Sentences', 'SentencesLocaleIdIndex'),
+
+    await queryInterface.addIndex(
+      'Sentences',
+      ['localeId'],
+      {
+        indexName: 'sentences_locale_id',
+        indicesType: 'FULLTEXT',
+      }
+    );
+  },
+  down: (queryInterface) => queryInterface.removeIndex('Sentences', 'sentences_locale_id'),
 };
