4a2ea4e96b2acc12c379c205225b50eee3252016
CapitainFlam <31761317+CapitainFlam@users.noreply.github.com>
2022-09-15 12:43:40 +0200

    Update fr.js
    as requested, only keeping FR and removing EN error description...
    But adding EN comment, for ^FR readers sake.

--- server/lib/validation/languages/fr.js
+++ server/lib/validation/languages/fr.js
@@ -1,3 +1,5 @@
+//see : https://github.com/common-voice/sentence-collector/blob/main/server/lib/validation/VALIDATION.md for more information
+
 const tokenizeWords = require('talisman/tokenizers/words');
 
 // Minimum of words that qualify as a sentence. 
@@ -7,24 +9,27 @@ const MIN_WORDS = 1;
 const MAX_WORDS = 14;
 
 const INVALIDATIONS = [{
+  //[min..max] words
   fn: (sentence) => {
     const words = tokenizeWords(sentence);
     return words.length < MIN_WORDS || words.length > MAX_WORDS;
   },
-  error: `Number of words must be between ${MIN_WORDS} and ${MAX_WORDS} (inclusive) - Le nombre de mot doit être entre ${MIN_WORDS} et ${MAX_WORDS} (inclus)`,
+  error: `Le nombre de mots doit être entre ${MIN_WORDS} et ${MAX_WORDS} (inclus)`,
 }, {
+  //no numbers
   regex: /[0-9]+/,
-  error: 'Sentence should not contain numbers - les phrases ne doivent pas contenir de nombres',
+  error: 'Les phrases ne doivent pas contenir de nombres',
 }, {
+  //no symbols
   regex: /[<>+*#@%^[\]()/]/,
-  error: 'Sentence should not contain symbols - les phrases de doivent pas contenir de symboles \(\*, \#, \(, etc\)',
+  error: 'Les phrases de doivent pas contenir de symboles (*, #, (, etc.)',
 }, {
   // Any words consisting of uppercase letters or uppercase letters with a period
   // inbetween are considered abbreviations or acronyms.
   // This currently also matches fooBAR but we most probably don't want that either
   // as users wouldn't know how to pronounce the uppercase letters.
   regex: /[A-Z]{2,}|[A-Z]+\.*[A-Z]+/,
-  error: 'Sentence should not contain abbreviations - Les phrases ne doivent pas contenir des abréviations ou sigles',
+  error: 'Les phrases ne doivent pas contenir des abréviations ou sigles',
 }];
 
 module.exports = {
